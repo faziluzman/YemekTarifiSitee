48d3c6810da4adf12eddb437ec21ddc7c78dc637
Fazıl Uzman <fazl.uzman1@gmail.com>
2022-08-29 05:41:38 +0300

    Add project files.

---
+++ YemekTarifiSitee/App_Code/sqlsinifi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for sqlsinifi
+/// </summary>
+public class sqlsinifi
+{
+    public SqlConnection baglanti()
+    {
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-3I6BO52\\SQLEXPRESS;Initial Catalog=Dbo-yemektarifi;Integrated Security=True");
+        baglan.Open();
+        return baglan;
+    }
+}
