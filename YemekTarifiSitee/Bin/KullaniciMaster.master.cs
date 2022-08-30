86f457b976e7e3bf0d0cd14d7e6af171e65304e8
Fazıl Uzman <fazl.uzman1@gmail.com>
2022-08-30 22:08:51 +0300

    Kategorileri ekledim

---
+++ YemekTarifiSitee/Bin/KullaniciMaster.master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+public partial class Bin_KullaniciMaster : System.Web.UI.MasterPage
+{
+    sqlsinifi bgl = new sqlsinifi();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlCommand komut = new SqlCommand("select * from Tbl_Kategoriler",bgl.baglanti());
+        SqlDataReader oku=komut.ExecuteReader();
+        DataList1.DataSource= oku;
+        DataList1.DataBind();
+    }
+}
