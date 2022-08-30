cb900e5748a853214d560b32bc1d8c307fdd2308
Fazıl Uzman <fazl.uzman1@gmail.com>
2022-08-31 01:27:37 +0300

    kullanıcı admine tarif öneriyor

--- YemekTarifiSitee/TarifOner.aspx.cs
+++ YemekTarifiSitee/TarifOner.aspx.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 public partial class TarifOner : System.Web.UI.Page
 {
+    sqlsinifi bgl = new sqlsinifi();
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlCommand komut = new SqlCommand("insert into Tbl_Tarifler (TarifAd,TarifMalzeme,TarifYapilis,TarifResim,TarifSahip,TarifSahipMail) values (@t1,@t2,@t3,@t4,@t5,@t6)", bgl.baglanti());
+        komut.Parameters.AddWithValue("@t1", TextBox1.Text);
+        komut.Parameters.AddWithValue("@t2", TextBox2.Text);
+        komut.Parameters.AddWithValue("@t3", TextBox3.Text);
+        komut.Parameters.AddWithValue("@t4", FileUpload1.FileName);
+        komut.Parameters.AddWithValue("@t5", TextBox4.Text);
+        komut.Parameters.AddWithValue("@t6", TextBox5.Text);
+        komut.ExecuteNonQuery();
+        bgl.baglanti().Close();
+        Response.Write("Başarılı");
     }
 }
